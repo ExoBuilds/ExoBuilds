a65124a40d51fba4a929a5fbf10e6f6f32e24f29
Guismus <guillaume.boudaille@epitech.eu>
2022-05-28 17:56:27 +0200

    New main.rs

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,19 +1,29 @@
 #[macro_use] extern crate rocket;
 
+use rocket_dyn_templates::Template;
+use std::collections::HashMap;
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    let context: HashMap<String, String> = HashMap::new();
+    Template::render("index", &context)
 }
 
-#[get("/hello/<name>")]
-fn hello(name: &str) -> String {
-    format!("Hello, {}!", name)
+#[get("/tkt")]
+fn tkt() -> Template {
+    let context: HashMap<String, String> = HashMap::new();
+    Template::render("index", &context)
 }
 
+#[get("/<word>")]
+fn test(word: &str) -> String {
+    format!("J'aime les {}", word)
+}
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
+        .mount("/arabe", routes![test, tkt])
         .mount("/", routes![index])
-        .mount("/", routes![hello])
+        .attach(Template::fairing())
 }
