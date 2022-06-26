c8bae6280e1ec891658a864c1ff1c46c336b2cf2
RqndomHax <paul.codepro@gmail.com>
2022-06-26 17:02:29 +0200

    :lipstick: added button to look for a specific player

--- app/public/js/search.js
+++ app/public/js/search.js
@@ -1,7 +1,9 @@
 function search_champ()
 {
+    
     list = document.getElementsByClassName("champion-icon");
     value = document.getElementById("search-champ").value.toLowerCase()
+    document.getElementById("searchplayer").href = "./profile/" + value
     for (i = 0; list[i]; i++) {
         if (list[i].id.toLowerCase().includes(value))
             list[i].hidden = false;
